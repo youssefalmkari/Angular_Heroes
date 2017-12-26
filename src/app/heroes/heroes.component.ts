eef29dea1b91fc2cac72bb471eadcb98d4eaabf1
fireyose <yose@Youssefs-MBP.home>
2017-12-26 16:57:13 -0500

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  sort(heroes: Hero[]): void {
+    this.heroes.sort();
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+    this.sort(this.heroes);
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Superman' },
+    { id: 15, name: 'Batman' },
+    { id: 16, name: 'The Flash' },
+    { id: 17, name: 'Wolverine' },
+    { id: 18, name: 'Cyclops' },
+    { id: 19, name: 'The Hulk' },
+    { id: 20, name: 'Silver Surfer' }
+];
